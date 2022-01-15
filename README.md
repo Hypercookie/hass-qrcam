381f6527430029e381767ba4dcbbe487a1c37ca4
Jannes Müller <jannes.mueller@uni-konstanz.de>
2022-01-15 21:49:30 +0100

    added color and shape options

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 [![hacs][hacsbadge]][hacs]
 ![Project Maintenance][maintenance-shield]
 
-This custom component creates cameras displaying qrcodes. The QRCodes can be static or generated from templates. 
-If you use a template as content the camera will update with a new qr code when the template updates. 
+This custom component creates cameras displaying qrcodes. The QRCodes can be static or generated from templates.
+If you use a template as content the camera will update with a new qr code when the template updates.
 
 **This component will set up the following platforms.**
 
@@ -18,7 +18,7 @@ Platform | Description
 
 
 ## Configuration
- 
+
 Example `configuration.yaml` entry:
 
 ```
@@ -59,9 +59,14 @@ Name | Description |Required | Type | Default
 `version` | The "Version" of the qr code (which is basically the size) | No | int between 1 and 40 | Auto
 `error_correction` | How much error correction the QRCode should have | No | see Error Corretion | ERROR_CORRECT_M
 `box_size` | The size of the Black Boxes in pixel | No | Positive Integer | 10
-`border` | Border arround the code | No | Integer > 3 | 4 
+`border` | Border arround the code | No | Integer > 3 | 4
 `fill_color`| RGB Color for the (normaly black) parts of the QRCode | No | String of form "r,g,b" | "0,0,0"
 `back_color`| RGB Color for the background of the QRCode | No | String of form "r,g,b" | "255,255,255"
+`edge_color`| RGB Color for the edge of the qr code (only used if color_mask is radial or square) | No | String of form "r,b,g" | "0,0,0"
+`left_color`| RGB Color for the left side of the qr code (only used if color_mask is horizontal or vertical) | No | String of form "r,g,b" | "0,0,0"
+`right_color`| RGB Color for the right side of the qr code (only used if color_mask is horizontal or vertical) | No | String of form "r,g,b" | "0,0,0"
+`color_mask`| Mask for the Colors of the QR Code | No | See Color Mask | "solid"
+`drawer` | Options on how to render the QR Code | No | See Rendering | "square"
 
 ## Error Correction
 The error_correction parameter controls the error correction used for the QR Code.
@@ -70,9 +75,30 @@ Name | Discription
 ERROR_CORRECT_L | About 7% or less errors can be corrected.
 ERROR_CORRECT_M | About 15% or less errors can be corrected.
 ERROR_CORRECT_Q | About 25% or less errors can be corrected.
-ERROR_CORRECT_H | About 30% or less errors can be corrected. 
+ERROR_CORRECT_H | About 30% or less errors can be corrected.
 ***
+## Color Mask
+![masks](https://github.com/lincolnloop/python-qrcode/blob/master/doc/color_masks.png)
+Name | Number in Image
+-- | --
+`solid`| 1
+`radial`| 2
+`square`| 3
+`horizontal` | 4
+`vertical`| 5
+ - (WIP) | 6
 
+## Drawer
+![drawers](https://github.com/lincolnloop/python-qrcode/blob/master/doc/module_drawers.png)
+Name | Number in Image
+-- | --
+`square`| 1
+`gapped_square`| 2
+`circle`| 3
+`rounded`| 4
+`vertical_bars`| 5
+`horizontal_graphs`| 6
+Name |
 [commits]: https://github.com/Hypercookie/hass-qrcam/commits/master
 [hacs]: https://github.com/custom-components/hacs
 [hacsbadge]: https://img.shields.io/badge/HACS-Custom-orange.svg?style=for-the-badge
--- custom_components/qrcam/camera.py
+++ custom_components/qrcam/camera.py
@@ -11,66 +11,104 @@ import voluptuous as vol
 import re
 from homeassistant.const import CONF_NAME
 import homeassistant.helpers.config_validation as cv
-from homeassistant.components.camera import (PLATFORM_SCHEMA,SUPPORT_STREAM,Camera)
+from homeassistant.components.camera import PLATFORM_SCHEMA, SUPPORT_STREAM, Camera
 from homeassistant.core import Config, HomeAssistant
 from homeassistant.exceptions import TemplateError
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType,DiscoveryInfoType
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from homeassistant.helpers.reload import async_setup_reload_service
-ERROR_CORRECT_VALUES = {"ERROR_CORRECT_L": 1,"ERROR_CORRECT_M":0,"ERROR_CORRECT_Q":3,"ERROR_CORRECT_H":2}
-DRAWERS = {"square": SquareModuleDrawer() ,"gapped_square": GappedSquareModuleDrawer(),"circle":CircleModuleDrawer(),"rounded":RoundedModuleDrawer(),"vertical_bars":VerticalBarsDrawer(),"horizontal_graphs": HorizontalBarsDrawer()}
-COLOR_MASKS = {"solid" : SolidFillColorMask(),"radial" : RadialGradiantColorMask() ,"square":SquareGradiantColorMask(),"horizontal":HorizontalGradiantColorMask(),"vertical":VerticalGradiantColorMask()}
+
+ERROR_CORRECT_VALUES = {
+    "ERROR_CORRECT_L": 1,
+    "ERROR_CORRECT_M": 0,
+    "ERROR_CORRECT_Q": 3,
+    "ERROR_CORRECT_H": 2,
+}
+DRAWERS = {
+    "square": SquareModuleDrawer(),
+    "gapped_square": GappedSquareModuleDrawer(),
+    "circle": CircleModuleDrawer(),
+    "rounded": RoundedModuleDrawer(),
+    "vertical_bars": VerticalBarsDrawer(),
+    "horizontal_graphs": HorizontalBarsDrawer(),
+}
+COLOR_MASKS = {
+    "solid": SolidFillColorMask(),
+    "radial": RadialGradiantColorMask(),
+    "square": SquareGradiantColorMask(),
+    "horizontal": HorizontalGradiantColorMask(),
+    "vertical": VerticalGradiantColorMask(),
+}
 _LOGGER = logging.getLogger(__name__)
-from . import DOMAIN,PLATFORMS
+from . import DOMAIN, PLATFORMS
 
 
 def error_correct(value: Any) -> str:
     if value not in ERROR_CORRECT_VALUES:
-        raise vol.Invalid("Error Correction Unkown should be one of [\"ERROR_CORRECT_L\",\"ERROR_CORRECT_M\",\"ERROR_CORRECT_Q\",\"ERROR_CORRECT_H\"]")
+        raise vol.Invalid(
+            'Error Correction Unkown should be one of ["ERROR_CORRECT_L","ERROR_CORRECT_M","ERROR_CORRECT_Q","ERROR_CORRECT_H"]'
+        )
     else:
         return str(value)
+
+
 def version(value: Any) -> int:
     if value is None:
         return None
     if value > 40 or value < 1:
         raise vol.Invalid("Error version has to be between 1 and 40")
     return int(value)
-def boxSize(value : Any) -> int:
-    if type(value) == int and value>0:
+
+
+def boxSize(value: Any) -> int:
+    if type(value) == int and value > 0:
         return value
     else:
         raise vol.Invalid("No Float or negative numbers allowed")
-def border(value : Any) -> int:
-    if type(value) == int and value>=4:
+
+
+def border(value: Any) -> int:
+    if type(value) == int and value >= 4:
         return value
     else:
         raise vol.Invalid("Value must be greater then 4 and no float")
-def color(value : Any) -> tuple(int):
-    if(re.match(" *\d{1,3} *, *\d{1,3} *, *\d{1,3} *",str(value))):
-        return tuple(map(lambda x : int(x),str(value).replace(" ","").split(",")))
-    else :
+
+
+def color(value: Any) -> tuple(int):
+    if re.match(" *\d{1,3} *, *\d{1,3} *, *\d{1,3} *", str(value)):
+        return tuple(map(lambda x: int(x), str(value).replace(" ", "").split(",")))
+    else:
         raise vol.Invalid("Color did not match '*\d{1,3} *, *\d{1,3} *, *\d{1,3} *'")
-def drawer(value : Any) -> str:
-    if(value in DRAWERS):
+
+
+def drawer(value: Any) -> str:
+    if value in DRAWERS:
         return value
+
+
 def colormask(value: Any) -> str:
-    if(value in COLOR_MASKS):
+    if value in COLOR_MASKS:
         return value
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required("content") : cv.template,
-    vol.Required(CONF_NAME): cv.string,
-    vol.Optional("version", default = None) : version,
-    vol.Optional("error_correction",default ="ERROR_CORRECT_M") : error_correct,
-    vol.Optional("box_size",default =10) : boxSize,
-    vol.Optional("border",default =4) : border,
-    vol.Optional("fill_color",default = "0,0,0") : color,
-    vol.Optional("left_color",default="0,0,0") : color,
-    vol.Optional("right_color",default="0,0,0"):color,
-    vol.Optional("back_color",default = "255,255,255") : color,
-    vol.Optional("edge_color",default = "255,255,255") : color,
-    vol.Optional("drawer",default=None) : drawer,
-    vol.Optional("color_mask",default= None) : colormask
-})
+
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required("content"): cv.template,
+        vol.Required(CONF_NAME): cv.string,
+        vol.Optional("version", default=None): version,
+        vol.Optional("error_correction", default="ERROR_CORRECT_M"): error_correct,
+        vol.Optional("box_size", default=10): boxSize,
+        vol.Optional("border", default=4): border,
+        vol.Optional("fill_color", default="0,0,0"): color,
+        vol.Optional("left_color", default="0,0,0"): color,
+        vol.Optional("right_color", default="0,0,0"): color,
+        vol.Optional("back_color", default="255,255,255"): color,
+        vol.Optional("edge_color", default="255,255,255"): color,
+        vol.Optional("drawer", default="square"): drawer,
+        vol.Optional("color_mask", default="solid"): colormask,
+    }
+)
+
 
 async def async_setup_platform(
     hass: HomeAssistant,
@@ -79,12 +117,14 @@ async def async_setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up qr cam"""
-    await async_setup_reload_service(hass,DOMAIN,PLATFORMS)
-    async_add_entities([QRCAM(config,hass)])
+    await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
+    async_add_entities([QRCAM(config, hass)])
+
 
 class QRCAM(Camera):
     """QR CAM"""
-    def __init__(self,config,hass) -> None:
+
+    def __init__(self, config, hass) -> None:
         super().__init__()
         self._hass = hass
         self._name = config[CONF_NAME]
@@ -101,43 +141,54 @@ class QRCAM(Camera):
         self._left_color = config["left_color"]
         self._drawer = config["drawer"]
         self._color_mask = config["color_mask"]
-        self._drawer = SquareModuleDrawer() if self._drawer is None else DRAWERS[self._drawer]
-        self._color_mask = SolidFillColorMask() if self._color_mask is None else COLOR_MASKS[self._color_mask]
+        self._drawer = (
+            SquareModuleDrawer() if self._drawer is None else DRAWERS[self._drawer]
+        )
+        self._color_mask = (
+            SolidFillColorMask()
+            if self._color_mask is None
+            else COLOR_MASKS[self._color_mask]
+        )
+
     @property
     def supported_features(self) -> int:
         return 0
+
     async def async_camera_image(
-        self, width: int | None = None, height : int | None = None
+        self, width: int | None = None, height: int | None = None
     ) -> bytes | None:
         """Still Image"""
         try:
 
-          cont =   self._content.async_render(parse_result=False)
-          qr = qrcodegen.QRCode(
-            version=self._version,
-            error_correction=ERROR_CORRECT_VALUES[self._error_correction],
-            box_size=self._box_size,
-            border=self._border,
-          )
-          qr.add_data(cont)
-          qr.make(fit=True)
-          img = qr.make_image(back_color=self._back_color,
-                              fill_color=self._fill_color,
-                              edge_color = self._edge_color,
-                              right_color = self._right_color,
-                              left_color = self._left_color,
-                              image_factory=StyledPilImage,
-                              module_drawer = self._drawer,
-                              color_mask = self._color_mask)
-          img_byte_array = io.BytesIO()
-          img.save(img_byte_array,format="PNG")
-          return img_byte_array.getvalue()
+            cont = self._content.async_render(parse_result=False)
+            qr = qrcodegen.QRCode(
+                version=self._version,
+                error_correction=ERROR_CORRECT_VALUES[self._error_correction],
+                box_size=self._box_size,
+                border=self._border,
+            )
+            qr.add_data(cont)
+            qr.make(fit=True)
+            img = qr.make_image(
+                back_color=self._back_color,
+                fill_color=self._fill_color,
+                edge_color=self._edge_color,
+                right_color=self._right_color,
+                left_color=self._left_color,
+                image_factory=StyledPilImage,
+                module_drawer=self._drawer,
+                color_mask=self._color_mask,
+            )
+            img_byte_array = io.BytesIO()
+            img.save(img_byte_array, format="PNG")
+            return img_byte_array.getvalue()
         except TemplateError as err:
-           _LOGGER.error("Error parsing template %s: %s", self._still_image_url, err)
-           return None
+            _LOGGER.error("Error parsing template %s: %s", self._still_image_url, err)
+            return None
 
     @property
     def name(self):
         return self._name
+
     def is_recording(self) -> bool:
         return True
